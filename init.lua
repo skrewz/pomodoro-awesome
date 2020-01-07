d94cde971a3ec7441844a9d522f2c0f38d1de719
Anders Breindahl <skrewz@skrewz.net>
2020-01-07 11:51:40 +1100

    For my own purposes: retab'ed plus adding "renotify_timer".

--- init.lua
+++ init.lua
@@ -13,74 +13,85 @@ local pomodoro_image_path = beautiful.pomodoro_icon or awful.util.getdir("config
 
 -- setup widget
 local pomodoro = wibox.widget({
-      image = pomodoro_image_path,
-      widget = wibox.widget.imagebox
+  image = pomodoro_image_path,
+  widget = wibox.widget.imagebox
 })
 
 -- setup timers
-local pomodoro_timer         = gears.timer({ timeout = pomodoro_time })
-local pomodoro_tooltip_timer = gears.timer({ timeout = 1 })
-local pomodoro_nbsec         = 0
+local pomodoro_timer          = gears.timer({ timeout = pomodoro_time })
+local pomodoro_renotify_timer = gears.timer({ timeout = 3*60 })
+local pomodoro_tooltip_timer  = gears.timer({ timeout = 1 })
+local pomodoro_nbsec          = 0
 
 local function pomodoro_start()
-    pomodoro_timer:start()
-    pomodoro_tooltip_timer:start()
-    pomodoro.bg = beautiful.bg_normal
- end
+  pomodoro_timer:start()
+  pomodoro_renotify_timer:start()
+  pomodoro_tooltip_timer:start()
+  pomodoro.bg = beautiful.bg_normal
+end
 
 local function pomodoro_stop()
-   pomodoro_timer:stop(pomodoro_timer)
-   pomodoro_tooltip_timer:stop(pomodoro_tooltip_timer)
-   pomodoro_nbsec = 0
+  pomodoro_timer:stop(pomodoro_timer)
+  pomodoro_renotify_timer:stop(pomodoro_renotify_timer)
+  pomodoro_tooltip_timer:stop(pomodoro_tooltip_timer)
+  pomodoro_nbsec = 0
 end
 
 local function pomodoro_end()
-    pomodoro_stop()
-    pomodoro.bg = beautiful.bg_urgent
+  pomodoro_stop()
+  pomodoro.bg = beautiful.bg_urgent
 end
 
 local function pomodoro_notify(text)
-   naughty.notify({ title = "Pomodoro", text = text, timeout = 10,
-                    icon = pomodoro_image_path, icon_size = 64,
-                    width = 200
-                 })
+  naughty.notify({ title = "Pomodoro", text = text, timeout = 10,
+  icon = pomodoro_image_path, icon_size = 64,
+  width = 200
+})
 end
 
+pomodoro_renotify_timer:connect_signal("timeout",
+function(c)
+  if pomodoro_timer.started then
+    r = (pomodoro_time - pomodoro_nbsec) % 60
+    pomodoro_notify('End in ' .. math.floor((pomodoro_time - pomodoro_nbsec) / 60) .. ' min ' .. r)
+  end
+end)
+
 pomodoro_timer:connect_signal("timeout",
-                          function(c)
-                             pomodoro_end()
-                             pomodoro_notify('Ended')
-                          end)
+function(c)
+  pomodoro_end()
+  pomodoro_notify('Ended')
+end)
 
 pomodoro_tooltip_timer:connect_signal("timeout",
-                                  function(c)
-                                     pomodoro_nbsec = pomodoro_nbsec + 1
-                                  end)
+function(c)
+  pomodoro_nbsec = pomodoro_nbsec + 1
+end)
 
 pomodoro_tooltip = awful.tooltip({
-                                    objects = { pomodoro },
-                                    timer_function = function()
-                                                        if pomodoro_timer.started then
-                                                           r = (pomodoro_time - pomodoro_nbsec) % 60
-                                                           return 'End in ' .. math.floor((pomodoro_time - pomodoro_nbsec) / 60) .. ' min ' .. r
-                                                        else
-                                                           return 'pomodoro not started'
-                                                        end
-                                                     end,
+  objects = { pomodoro },
+  timer_function = function()
+    if pomodoro_timer.started then
+      r = (pomodoro_time - pomodoro_nbsec) % 60
+      return 'End in ' .. math.floor((pomodoro_time - pomodoro_nbsec) / 60) .. ' min ' .. r
+    else
+      return 'pomodoro not started'
+    end
+  end,
 })
 
 local function pomodoro_start_timer()
-   if not pomodoro_timer.started then
-      pomodoro_start()
-      pomodoro_notify('Started')
-   else
-      pomodoro_stop()
-      pomodoro_notify('Canceled')
-   end
+  if not pomodoro_timer.started then
+    pomodoro_start()
+    pomodoro_notify('Started')
+  else
+    pomodoro_stop()
+    pomodoro_notify('Canceled')
+  end
 end
 
 pomodoro:buttons(awful.util.table.join(
-                    awful.button({ }, 1, pomodoro_start_timer)
-              ))
+awful.button({ }, 1, pomodoro_start_timer)
+))
 
 return pomodoro
